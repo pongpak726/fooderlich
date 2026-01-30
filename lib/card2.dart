cef58fb7d3f75ee39776fd73a32a6e1430dbf8c8
pongpak726 <fuza2560@gmail.com>
2026-01-30 14:24:20 +0700

    commit01

---
+++ lib/card2.dart
@@ -0,0 +1,57 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:fooderlich/fooderlich_theme.dart';
+
+class Card2 extends StatelessWidget {
+  const Card2({super.key});
+  
+  final String category = 'Editors\'s Choice';
+  final String title = 'The Art of Dough';
+  final String description = 'Learn to make the perfect bread.';
+  final String chef = 'Ray wenderlich';
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Center(child: Container(
+      padding: const EdgeInsets.all(16),
+      constraints: const BoxConstraints(),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/mag.jpg') ,
+          fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0))
+      ),
+      
+      child: Stack(
+  children: [
+    Positioned(
+      top: 16,
+      child: Text(category,
+        style: FooderlichTheme.darkTextTheme.bodyLarge),
+    ),
+    Positioned(
+      top: 48,
+      child: Text(title,
+        style: FooderlichTheme.darkTextTheme.titleSmall),
+    ),
+    Positioned(
+      bottom: 48,
+      child: Text(description,
+        style: FooderlichTheme.darkTextTheme.bodyLarge),
+    ),
+    Positioned(
+      bottom: 16,
+      right: 16,
+      child: Text(chef,
+        style: FooderlichTheme.darkTextTheme.bodyLarge),
+    ),
+  ],
+)
+    
+    ),
+    );
+  }
+  }
---
+++ lib/home.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/card1.dart';
+import 'package:fooderlich/card2.dart';
+import 'package:fooderlich/card3.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+   @override
+  State<StatefulWidget> createState() => _HomeState() ;
+}
+  
+
+  class _HomeState extends State<Home> {
+    int _selectedIndex = 0;
+
+    static List<Widget> pages = <Widget>[
+      Card1(),
+      Card2(),
+      Card3(),
+    ];
+
+    void _onItemTapped(int index){
+      setState((){
+        _selectedIndex = index;
+      });
+    }
+
+    @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text(
+        'Fooderlich',
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+    ),
+    body: pages[_selectedIndex],
+    bottomNavigationBar: BottomNavigationBar(
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.card_giftcard),
+          label: 'Card 1',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.card_giftcard),
+          label: 'Card 2',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.card_giftcard),
+          label: 'Card 3',
+        ),
+      ],
+    ),
+  );
+}
+
+  }
+  
+ 
